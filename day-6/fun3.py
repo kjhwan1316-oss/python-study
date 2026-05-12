3450e153ac56c3f8c3cf55426530e4cdc50ab950
juhwan <kjhwan1316@gmail.com>
2026-05-12 09:43:24 +0900

    함수 실습

---
+++ day-6/fun3.py
@@ -0,0 +1,12 @@
+def dis_price(a, b):
+    return a - (a/b)
+
+
+
+#a상품: 10000원, 할인율: 10%
+price_a = dis_price(10000,10)
+print(price_a) #9000
+
+#b상품 가격: 50000원, 할인율: 20%
+price_b = dis_price(50000,20)
+print(price_b) #40000
